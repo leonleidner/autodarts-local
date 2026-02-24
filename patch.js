87feb0da4a2af4db23b481879d1045456a50325c
leonleidner <leidnerleon@gmail.com>
2026-02-24 16:43:14 +0100

    Remove unused patch files and empty scripts directory

--- patch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('server.js', 'utf8');
-code = code.replace(
-    /res\.json\(response\.data\);/,
-    `console.log("Board Data:", response.data); res.json(response.data);`
-);
-fs.writeFileSync('server.js', code);
